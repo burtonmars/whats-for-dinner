d1fac9ccea588a7d1ab42998a18b249deb2bbb15
UBC Student <burtonm1@students.cs.ubc.ca>
2023-11-19 15:30:57 -0800

    install daisyui and prisma

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
-import Image from 'next/image'
+import Link from 'next/link'
+import MealCard from './components/MealCard'
 
 export default function Home() {
   return (
-   <></>
+   <main>
+    <Link href="/users">users</Link>
+    <MealCard />
+   </main>
   )
 }
