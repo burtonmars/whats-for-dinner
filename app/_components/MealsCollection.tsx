5331ab1a40314bd47d9cc366a6be21daca340eef
UBC Student <burtonmars91@gmail.com>
2024-05-12 09:47:22 -0700

    feature: add laoding spinner while meals collection is fetched

--- app/_components/MealsCollection.tsx
+++ app/_components/MealsCollection.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+'use client';
+
+import React, { useState, useEffect } from 'react'
 
 import MealCard from './MealCard'
 import { deleteMeal } from '../_lib/data'
@@ -11,30 +13,41 @@ interface MealsCollectionProps {
 
 const MealsCollection = ({ meals, tag }: MealsCollectionProps) => {
   const focussedTag = tag ? tag : null;
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (meals.length > 0) {
+        setLoading(false);
+    }
+  }, [meals]);
+
   return (
     <>
-        <div className='flex md:hidden flex-col justify-center items-center'>
-            {meals.map((meal: Meal) =>
-                <MealCard 
-                key={meal.id} 
-                meal={meal} 
-                deleteMeal={deleteMeal}
-                focussedTag={focussedTag}/>
-            )}
-        </div>
-        <div className="hidden w-full h-full md:flex justify-center">
-            <div className='w-full xl:w-4/5 lg:mt-36 grid justify-center gap-12'>
-                <div className='grid grid-cols-mealCards md:grid-cols-mealCardsMd xl:grid-cols-mealCardsXl gap-y-10 xl:gap-y-16 w-fit'>
-                    {meals.map((meal: Meal) =>
-                        <MealCard
+        {loading ? 
+        <div className='flex my-36 h-96 justify-center items-center'>
+            <span className="loading loading-ring loading-lg"></span>
+        </div> : 
+        <>
+            <div className='flex md:hidden flex-col justify-center items-center'>
+                {meals.map((meal: Meal) => <MealCard
+                    key={meal.id}
+                    meal={meal}
+                    deleteMeal={deleteMeal}
+                    focussedTag={focussedTag} />
+                )}
+            </div><div className="hidden w-full h-full md:flex justify-center">
+                <div className='w-full xl:w-4/5 lg:mt-36 grid justify-center gap-12'>
+                    <div className='grid grid-cols-mealCards md:grid-cols-mealCardsMd xl:grid-cols-mealCardsXl gap-y-10 xl:gap-y-16 w-fit'>
+                        {meals.map((meal: Meal) => <MealCard
                             key={meal.id}
                             meal={meal}
                             deleteMeal={deleteMeal}
-                            focussedTag={focussedTag}/>
-                    )}
+                            focussedTag={focussedTag} />
+                        )}
+                    </div>
                 </div>
             </div>
-        </div>
+        </>}
     </>
   )
 }
